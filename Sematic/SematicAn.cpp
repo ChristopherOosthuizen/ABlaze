2e778c4806e273dae62fdfac70f18e21b2851e01
Christopher Oosthuizen <Christopher.jan.Oosthuizen@gmail.com>
2021-01-29 09:29:26 -0600

    added Sematics

---
+++ Sematic/SematicAn.cpp
@@ -0,0 +1,3 @@
+//The purpose of this file is to symatically analize a AST
+
+#include "SematicAn.h"
---
+++ Sematic/SematicAn.h
@@ -0,0 +1,9 @@
+// Anlize the Abstract sytntax tree using sematics.
+
+#ifndef ABLAZE_SematicAn_h
+#define ABLAZE_SematicAn_h
+
+class SematicAn{
+
+};
+#endif
